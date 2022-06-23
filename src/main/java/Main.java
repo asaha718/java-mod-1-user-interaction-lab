e1ce2f7150b9a4541e090261868305386289a947
Anug Saha <anugsaha@gmail.com>
2022-06-23 12:01:28 -0400

    add input from user and conditions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        System.out.print("Enter your age here : ");
+        
+        try (Scanner scanner = new Scanner(System.in);) {
+            int age = 0; 
+            String ageInput = scanner.nextLine();
+            age = Integer.parseInt(ageInput); 
+
+            if (age > 18) { 
+                System.out.print("You are legal to do what your heart desires. Good luck");
+            }
+
+            if(age < 18){ 
+                System.out.print("That is so cute");
+            }
+
+            if( age == 18 ){ 
+                System.out.print("Congrats on 18");
+            }
+        } 
     }
 }
