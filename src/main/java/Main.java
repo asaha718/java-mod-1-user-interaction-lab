6c0b9e5b9fef9c57e306fcb4f8b5a6f6c776150d
Anug Saha <anugsaha@gmail.com>
2022-06-23 12:09:47 -0400

    add condition for under 10 yo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.print("Enter your age here : ");
-        
-        try (Scanner scanner = new Scanner(System.in);) {
-            int age = 0; 
-            String ageInput = scanner.nextLine();
-            age = Integer.parseInt(ageInput); 
 
-            if (age > 18) { 
+        try (Scanner scanner = new Scanner(System.in);) {
+            int age = 0;
+            String ageInput = scanner.nextLine();
+            age = Integer.parseInt(ageInput);
+
+            if (age > 18) {
                 System.out.print("You are legal to do what your heart desires. Good luck");
             }
 
-            if(age < 18){ 
-                System.out.print("That is so cute");
+            if (age < 18) {
+                if (age < 10) {
+                    System.out.print("That is even more cute");
+                } else {
+                    System.out.print("That is so cute");
+                }
             }
 
-            if( age == 18 ){ 
+            if (age == 18) {
                 System.out.print("Congrats on 18");
             }
-        } 
+        }
     }
 }
